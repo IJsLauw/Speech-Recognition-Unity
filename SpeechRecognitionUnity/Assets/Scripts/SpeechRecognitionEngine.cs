b0ab7927cde398f1d01e0183b37629f1fd13a9aa
Laurent de Goede <laurent@ijsfontein.nl>
2018-02-07 13:56:39 +0100

    Added sounds and stuff

--- SpeechRecognitionUnity/Assets/Scripts/SpeechRecognitionEngine.cs
+++ SpeechRecognitionUnity/Assets/Scripts/SpeechRecognitionEngine.cs
@@ -6,17 +6,23 @@ using UnityEngine.Windows.Speech;
 
 public class SpeechRecognitionEngine : MonoBehaviour
 {
-    public string[] keywords = new string[] { "Alfa", "Bravo", "Charlie", "Delta", "Echo", "Foxtrot", "Golf", "Hotel", "India", "Juliett", "Kilo", "Lima", "Mike", "November", "Oscar", "Papa", "Quebec", "Romeo", "Sierra", "Tango", "Uniform", "Victor", "Whiskey", "Xray", "Yankee", "Zulu" };
-    public string[] armyAlphabet = new string[] { "Alfa", "Bravo", "Charlie", "Delta", "Echo", "Foxtrot", "Golf", "Hotel", "India", "Juliett", "Kilo", "Lima", "Mike", "November", "Oscar", "Papa", "Quebec", "Romeo", "Sierra", "Tango", "Uniform", "Victor", "Whiskey", "Xray", "Yankee", "Zulu", "Wow", "Wauw", "Waw", "IJsfontein", "Fuck", "Shit", "Motel", "Pappa", "Poppa", "Wise key", "Sulu", "Silly", "Nice", "Lime", "Alfa Romeoo", "Tongo" };
-    public string[] armyAlphabetWithDistractions = new string[] { "Alfa", "Bravo", "Charlie", "Delta", "Echo", "Foxtrot", "Golf", "Hotel", "India", "Juliett", "Kilo", "Lima", "Mike", "November", "Oscar", "Papa", "Quebec", "Romeo", "Sierra", "Tango", "Uniform", "Victor", "Whiskey", "Xray", "Yankee", "Zulu", "Wow", "Wauw", "Waw", "IJsfontein", "Fuck", "Shit", "Motel", "Pappa", "Poppa", "Wise key", "Sulu", "Silly", "Nice", "Lime", "Alfa Romeoo", "Tongo" };
+    public string[] codes;
+    public string[] armyAlphabetWithDistractions = new string[] { "Alfa", "Bravo", "Charlie", "Delta", "Echo", "Foxtrot", "Golf", "Hotel", "India", "Juliett", "Kilo", "Lima", "Mike", "November", "Oscar", "Papa", "Quebec", "Romeo", "Sierra", "Tango", "Uniform", "Victor", "Whiskey", "Xray", "Yankee", "Zulu", "Wow", "Wauw", "Waw", "IJsfontein", "Fuck", "Shit", "Motel", "Pappa", "Poppa", "Wise key", "Sulu", "Silly", "Nice", "Lime", "Alfa Romeo", "Tongo" };
     public ConfidenceLevel confidence = ConfidenceLevel.Low;
     public float speed = 1;
 
+    public Image background;
     public Text results;
-    public Image target;
+    public Text[] targets;
+    public AudioClip doorOpenSound;
+    public AudioClip[] lockSounds;
+    public AudioClip[] positiveSounds;
+    public AudioClip[] negativeSounds;
+    public AudioSource soundPlayer;
 
     protected PhraseRecognizer recognizer;
-    protected string word = "right";
+    protected string word;
+    protected int codeIndex = 0;
 
     private void Start()
     {
@@ -25,37 +31,60 @@ public class SpeechRecognitionEngine : MonoBehaviour
             recognizer = new KeywordRecognizer(armyAlphabetWithDistractions, confidence);
             recognizer.OnPhraseRecognized += Recognizer_OnPhraseRecognized;
             recognizer.Start();
+
         }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            print("space key was pressed");
+            InsertNextCode();
+        }
+
     }
 
     private void Recognizer_OnPhraseRecognized(PhraseRecognizedEventArgs args)
     {
         word = args.text;
         results.text = "You said: <b>" + word + "</b>";
-    }
 
-    private void Update()
-    {
-        var x = target.transform.position.x;
-        var y = target.transform.position.y;
-
-        switch (word)
+        if (codes[codeIndex] == word)
         {
-            case "up":
-                y += speed;
-                break;
-            case "down":
-                y -= speed;
-                break;
-            case "left":
-                x -= speed;
-                break;
-            case "right":
-                x += speed;
-                break;
+            InsertNextCode();
+        }
+        else
+        {
+            soundPlayer.PlayOneShot(negativeSounds[Random.Range(0, negativeSounds.Length)]);
         }
 
-        target.transform.position = new Vector3(x, y, 0);
+    }
+
+    private void InsertNextCode()
+    {
+        targets[codeIndex].text = codes[codeIndex].Substring(0,1);
+        targets[codeIndex].gameObject.SetActive(true);
+        codeIndex++;
+
+
+        if (codeIndex == 9)
+        {
+            background.color = Color.green;
+            soundPlayer.PlayOneShot(lockSounds[1]);
+            soundPlayer.PlayOneShot(doorOpenSound);
+
+            results.text = "Door unlocked!";
+
+            recognizer.Stop();
+
+            soundPlayer.PlayOneShot(positiveSounds[Random.Range(0, positiveSounds.Length)]);
+        }
+        else
+        {
+            soundPlayer.PlayOneShot(lockSounds[0]);
+        }
+
     }
 
     private void OnApplicationQuit()
